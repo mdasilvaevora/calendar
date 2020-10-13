d9656c37ba2afe2d40d280ee2b9f1110512243b5
mdasilvaevora <mdasilvaevora@gmail.com>
2020-10-12 21:23:06 -0300

    Formik & Yup set up for form validation

--- src/components/reminder/ReminderForm.js
+++ src/components/reminder/ReminderForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import moment from 'moment';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/styles';
 import Form from 'react-bootstrap/Form';
 import Paper from '@material-ui/core/Paper';
 import {CirclePicker} from 'react-color';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -25,52 +26,21 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
+const ReminderFormSchema = Yup.object().shape({
+    text: Yup.string()
+                .max(30, 'Ooops, description is to long. Should be less than 30 characters.')
+                .required('You should write a description')
+})
+
 const days = moment.weekdaysShort();
 
 export default function ReminderForm({reminder, updateReminder}) {
     const classes = useStyles();
     const [updatedReminder, setUpdatedReminder] = React.useState(reminder);
     const isNewReminder = reminder.id === undefined;
-    
-    const handleStartTimeChange = evt => {
-        const updatedTime = evt.target.value;
-        if(updatedTime < moment().format('HH:mm')) return;
-        setUpdatedReminder({
-            ...updatedReminder,
-            startTime:updatedTime
-        })
-    }
 
-    const handleDayChange = evt => {
-        setUpdatedReminder({
-            ...updatedReminder,
-            day: evt.target.value
-        })
-    }
-
-    const handleColorChange = color => {
-        setUpdatedReminder({
-            ...updatedReminder,
-            color: color.hex
-        })
-    }
-    
-    const handleDescriptionChange = evt => {
-        setUpdatedReminder({
-            ...updatedReminder,
-            text: evt.target.value
-        })
-    }
-
-    const handleCityChange = evt => {
-        setUpdatedReminder({
-            ...updatedReminder,
-            city: evt.target.value
-        })
-    }
-
-    const handleSubmit = evt => {
-        updateReminder(updatedReminder)
+    const handleSubmit = values => {
+        updateReminder(values)
     }
 
     return (
@@ -80,70 +50,88 @@ export default function ReminderForm({reminder, updateReminder}) {
                     className={classes.colorDisplay}
                     style={{backgroundColor: updatedReminder.color}}
                 />
-                <Form>
-                    <Form.Row>
-                        <Form.Group style={{width: '100%'}}>
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control 
-                                type="text" 
-                                value={updatedReminder.text}
-                                placeholder="Enter description"
-                                onChange={handleDescriptionChange}/>
-                        </Form.Group>
-                    </Form.Row>
 
-                    <Form.Row>
-                        <Form.Group>
-                            <Form.Label>Day</Form.Label>
-                            <Form.Control 
-                                as="select"
-                                value={updatedReminder.day}
-                                onChange={handleDayChange}>
-                                {days.map((day,index) => (
-                                    <option key={index}>{day}</option>
-                                ))}
-                            </Form.Control>
-                        </Form.Group>
+                <Formik
+                    validationSchema={ReminderFormSchema}
+                    onSubmit={handleSubmit}
+                    initialValues={updatedReminder}
+                >
+                {({ values, errors, touched, handleSubmit, handleChange, setValues}) => (
+                    <Form noValidate onSubmit={handleSubmit} >
+                        <Form.Row>
+                            <Form.Group style={{width: '100%'}}>
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control 
+                                    name="text"
+                                    type="text" 
+                                    value={values.text}
+                                    placeholder="Enter description"
+                                    onChange={handleChange}
+                                    isInvalid={touched.text && errors.text}
+                                    isValid={touched.text && !errors.text}/>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.text}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Form.Row>
 
-                        <Form.Group style={{display:'flex', flexDirection:'column'}}>
-                            <Form.Label>Time</Form.Label>
-                            <input 
-                                style={{height: '100%'}}
-                                type="time" 
-                                value={updatedReminder.startTime} 
-                                onChange={handleStartTimeChange}/>
-                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group>
+                                <Form.Label>Day</Form.Label>
+                                <Form.Control 
+                                    name="day"
+                                    as="select"
+                                    value={values.day}
+                                    onChange={handleChange}>
+                                    {days.map((day,index) => (
+                                        <option key={index}>{day}</option>
+                                    ))}
+                                </Form.Control>
+                            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>City</Form.Label>
-                            <Form.Control 
-                                type="text" 
-                                value={updatedReminder.city}
-                                onChange={handleCityChange}/>
-                        </Form.Group>
-                    </Form.Row>
+                            <Form.Group style={{display:'flex', flexDirection:'column'}}>
+                                <Form.Label>Time</Form.Label>
+                                <input 
+                                    name="startTime"
+                                    style={{height: '100%'}}
+                                    type="time" 
+                                    value={values.startTime} 
+                                    onChange={handleChange}/>
+                            </Form.Group>
 
-                    <Form.Row>
-                        <Form.Group style={{width: '100%'}}>
-                        <Form.Label>Color</Form.Label>
-                            <CirclePicker 
-                                className={classes.colorPicker}
-                                color={reminder.color}
-                                onChangeComplete={handleColorChange}/>
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Button 
-                            variant="primary" 
-                            onClick={handleSubmit}
-                            style={{
-                                backgroundColor: updatedReminder.color,
-                                borderColor: updatedReminder.color,
-                                marginRight: '8px'}}>
-                            {isNewReminder? 'Crear' : 'Actualizar'}
-                        </Button>
-                    </Form.Row>
-                </Form>
+                            <Form.Group>
+                                <Form.Label>City</Form.Label>
+                                <Form.Control 
+                                    name="city"
+                                    type="text" 
+                                    value={values.city}
+                                    onChange={handleChange}/>
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                            <Form.Group style={{width: '100%'}}>
+                            <Form.Label>Color</Form.Label>
+                                <CirclePicker 
+                                    className={classes.colorPicker}
+                                    color={values.color}
+                                    onChangeComplete={color => setValues({color: color.hex})}/>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Button 
+                                variant="primary" 
+                                type="submit"
+                                style={{
+                                    backgroundColor: updatedReminder.color,
+                                    borderColor: updatedReminder.color,
+                                    marginRight: '8px'}}>
+                                {isNewReminder? 'Crear' : 'Actualizar'}
+                            </Button>
+                        </Form.Row>
+                    </Form>
+                )}
+                </Formik>
             </Paper>
         </div>
     )
